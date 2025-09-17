583b7852bf744dc95a909b48491629ef68291ed4
dedsechack-code <a.kapellas@significadigital.com>
2025-09-17 22:31:20 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,13 +22,14 @@ variable "fortios_token" {
 }
 
 # Minimal, harmless change: a test address object
-resource "fortios_firewall_address" "tofu_test_host4" {
-  name   = "tofu-test-host4"
+resource "fortios_firewall_address" "tofu_test_host5" {
+  name   = "tofu-test-host5"
   type   = "ipmask"
-  subnet = "10.10.30.10/32"
+  subnet = "10.10.50.10/32"
   # If you didn't set provider.vdom above, add this line instead:
   vdomparam = "root"
 }
+
 
 
 
